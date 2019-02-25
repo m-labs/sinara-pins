338352cff72377f9da27389f7235565dddb16909
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-02-25 19:05:17 +0800

    fix sdram iostandards

--- pin_utils.py
+++ pin_utils.py
@@ -56,26 +56,26 @@ def print_section(s):
 
 ddr3_template = """Subsignal("a", Pins(
     "{a}"),
-    IOStandard("SSTL15")),
-Subsignal("ba", Pins("{ba}"), IOStandard("SSTL15")),
-Subsignal("ras_n", Pins("{ras_n}"), IOStandard("SSTL15")),
-Subsignal("cas_n", Pins("{cas_n}"), IOStandard("SSTL15")),
-Subsignal("we_n", Pins("{we_n}"), IOStandard("SSTL15")),
-Subsignal("cs_n", Pins("{cs_n}"), IOStandard("SSTL15")),
+    IOStandard("SSTL15_DCI")),
+Subsignal("ba", Pins("{ba}"), IOStandard("SSTL15_DCI")),
+Subsignal("ras_n", Pins("{ras_n}"), IOStandard("SSTL15_DCI")),
+Subsignal("cas_n", Pins("{cas_n}"), IOStandard("SSTL15_DCI")),
+Subsignal("we_n", Pins("{we_n}"), IOStandard("SSTL15_DCI")),
+Subsignal("cs_n", Pins("{cs_n}"), IOStandard("SSTL15_DCI")),
 Subsignal("dm", Pins("{dm}"),
-    IOStandard("SSTL15")),
+    IOStandard("SSTL15_DCI")),
 Subsignal("dq", Pins(
     "{dq}"),
-    IOStandard("SSTL15_T_DCI")),
+    IOStandard("SSTL15_DCI")),
 Subsignal("dqs_p", Pins("{dqs_p}"),
-    IOStandard("DIFF_SSTL15")),
+    IOStandard("DIFF_SSTL15_DCI")),
 Subsignal("dqs_n", Pins("{dqs_n}"),
-    IOStandard("DIFF_SSTL15")),
-Subsignal("clk_p", Pins("{clk_p}"), IOStandard("DIFF_SSTL15")),
-Subsignal("clk_n", Pins("{clk_n}"), IOStandard("DIFF_SSTL15")),
-Subsignal("cke", Pins("{cke}"), IOStandard("SSTL15")),
-Subsignal("odt", Pins("{odt}"), IOStandard("SSTL15")),
-Subsignal("reset_n", Pins("{reset_n}"), IOStandard("LVCMOS15"))"""
+    IOStandard("DIFF_SSTL15_DCI")),
+Subsignal("clk_p", Pins("{clk_p}"), IOStandard("DIFF_SSTL15_DCI")),
+Subsignal("clk_n", Pins("{clk_n}"), IOStandard("DIFF_SSTL15_DCI")),
+Subsignal("cke", Pins("{cke}"), IOStandard("SSTL15_DCI")),
+Subsignal("odt", Pins("{odt}"), IOStandard("SSTL15_DCI")),
+Subsignal("reset_n", Pins("{reset_n}"), IOStandard("SSTL15"))"""
 
 
 def print_ddr3_decl(a, ba, ras_n, cas_n, we_n, cs_n, dm, dq, dqs, clk, cke, odt, reset_n):
